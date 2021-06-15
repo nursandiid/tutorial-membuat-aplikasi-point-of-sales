ea01d3c21ab58e441dceddb91aa6ea470d22e4d8
Nursandi <sandinur157@gmail.com>
2021-06-15 13:35:56 +0700

    replace arrow function with normal function

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn () => redirect()->route('login'));
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
